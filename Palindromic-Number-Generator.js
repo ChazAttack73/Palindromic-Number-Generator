211fa336deac541314888dd6e903426cc5643494
Chaz <cmtlum73@gmail.com>
2015-12-10 10:17:11 -1000

    Just starting

---
+++ Palindromic-Number-Generator.js
@@ -0,0 +1,15 @@
+function palindromeNumberGenerator ( number ) {
+    var isPalindrome = reversedNum;
+    var stepCounter = 0;
+
+    function checkPalindrome ( number ) {
+      var reversedNum = number.toString().split('').reverse().join('');
+      console.log(checkPalindrome(7658));
+    }
+
+    while (number !== reversedNum) {
+      var newNum = add(number + reversedNum);
+      newNum = number;
+      stepCounter++;
+    }
+}
